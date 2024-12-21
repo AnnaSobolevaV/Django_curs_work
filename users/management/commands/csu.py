d4807e9f7efcd281398d05361ea0ec01dcd4fae3
Anna Soboleva <asoboleva.avs@gmail.com>
2024-12-22 00:04:03 +0300

    structure initial structure

---
+++ users/management/commands/csu.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='admin_curs6@mail.pro',
+            first_name='Admin',
+            last_name='curs6_work',
+            is_staff=True,
+            is_superuser=True
+        )
+
+        user.set_password('superuser_pass')
+        user.save()
